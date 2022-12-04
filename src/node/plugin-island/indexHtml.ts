126fbaa881177ee0ecd32c1b1a42ba5ad1be13a4
qin.zehua <775935087@qq.com>
2022-12-04 20:08:55 +0800

    feat: 工程化配置
    配置eslint, prettier, commitlint, husky, lint-staged

--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,29 +1,29 @@
-import { readFile } from "fs/promises";
-import { Plugin } from "vite";
-import { DEFAULT_TEMPLATE_PATH, CLIENT_ENTRY_PATH } from "../constants";
+import { readFile } from 'fs/promises';
+import { Plugin } from 'vite';
+import { DEFAULT_TEMPLATE_PATH, CLIENT_ENTRY_PATH } from '../constants';
 
 export function pluginIndexHtml(): Plugin {
   return {
-    name: "xxxx",
+    name: 'xxxx',
     transformIndexHtml(html) {
       return {
         html,
         tags: [
           {
-            tag: "script",
+            tag: 'script',
             attrs: {
-              type: "module",
-              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+              type: 'module',
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
             },
-            injectTo: "body",
-          },
-        ],
+            injectTo: 'body'
+          }
+        ]
       };
     },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
-          let html = await readFile(DEFAULT_TEMPLATE_PATH, "utf-8");
+          let html = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8');
           try {
             html = await server.transformIndexHtml(
               req.url,
@@ -31,13 +31,13 @@ export function pluginIndexHtml(): Plugin {
               req.originalUrl
             );
             res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
+            res.setHeader('Content-Type', 'text/html');
             res.end(html);
           } catch (e) {
             return next(e);
           }
         });
       };
-    },
+    }
   };
 }
